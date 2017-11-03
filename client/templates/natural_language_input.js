b70c9204dcc1b7689ab9b6069f6ce4b899f37a7b
youralien <ryanlouie2021@u.northwestern.edu>
2017-11-03 16:40:06 -0400

    exclude place categories works front-end and back-end

--- client/templates/natural_language_input.js
+++ client/templates/natural_language_input.js
@@ -1,24 +1,19 @@
-import { exec } from 'child_process'
-
 Template.naturalLanguageInput.events({
   'submit form': function(e) {
     e.preventDefault();
 
     const queryString = $(e.target).find('[name=queryString]').val()
 
-    // SEARCH_YELP_SCRIPT = "/Users/ryan/NUDelta/affordanceaware/search_yelp_places.py"
-    // const cmd = `echo '${ queryString }' | python ${ SEARCH_YELP_SCRIPT }`;
-    // console.log(queryString);
-    // console.log(cmd);
-    // const categories = Meteor.wrapAsync(exec)(cmd);
-    // console.log(categories);
-
-
     var query = {
       query: queryString
     };
-    query._id = Queries.insert(query);
 
-    Router.go('queryBuilderPage', query);
+    Meteor.call('affordanceLanguageProcess', query, function (error, data) {
+      if (error)
+        return alert(error.reason);
+
+      Router.go('queryBuilderPage', {_id: data._id});
+
+    });
   }
 })
---
+++ client/templates/query_builder_page.js
@@ -0,0 +1,21 @@
+Template.queryBuilderPage.events({
+  'click .remove-category': function(e) {
+    const cat2rm = $(e.target)
+                   .parent()[0]
+                   .firstChild
+                   .textContent;
+
+    Queries.update(this._id, {
+      $addToSet: {excluded_categories : cat2rm}
+    });
+
+  }
+});
+
+Template.queryBuilderPage.helpers({
+  'includedCategories': function(allCats, excludeCats) {
+    return allCats.filter(function(item) { 
+      return !excludeCats.includes(item);
+    });
+  }
+})
