71632bfb1de932af148e78841b4a9e100373c705
youralien <ryanlouie2021@u.northwestern.edu>
2017-11-07 18:35:32 -0600

    Display the current lego composition + Use Lego for the composition works with movement blocks

--- client/templates/movement_block_builder.js
+++ client/templates/movement_block_builder.js
@@ -43,6 +43,16 @@ Template.movementBlockBuilder.events({
     const url = $(e.target).attr('href')
     const id = url.split('/').pop()
     Router.go('queryBuilderPage', {_id: id});
+  },
+
+  'click .btn-use-lego': function(e) {
+    lego_id = $(e.target).parent().attr('id');
+    query_id = $(e.target).attr('queryId')
+
+    // TODO: not sure about ands/ors
+    Queries.update(query_id, {
+      $addToSet: {"subLegos": lego_id}
+    });
   }
 
 
