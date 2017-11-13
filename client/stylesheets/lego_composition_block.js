cd1eceb8b2141aba1732ea158b3f345066bb43fd
youralien <ryanlouie2021@u.northwestern.edu>
2017-11-13 16:11:07 -0600

    fixed space/tabs + comments for PR

--- client/stylesheets/lego_composition_block.js
+++ client/stylesheets/lego_composition_block.js
@@ -1,8 +1,11 @@
 Template.legoCompositionBlock.events({
-	'click .x': function(e) {
+  'click .x': function(e) {
+    // if user clicks x button on the sublego, lego is removed from composition
+
     const query_id = $(e.target).parent().attr('queryId');
     const lego_id = $(e.target).parent().attr('sublegoId');
 
+    // remove this lego from subLegos field
     Queries.update(query_id, {
       $pull: {"subLegos": lego_id}
     });
