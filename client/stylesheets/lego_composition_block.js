3d5dcd2be4264dc7c5d6bfaf9e395e4fe0f7ec5a
youralien <ryanlouie2021@u.northwestern.edu>
2017-11-13 16:04:06 -0600

    x or canceling lego composition blocks is functional

--- client/stylesheets/lego_composition_block.js
+++ client/stylesheets/lego_composition_block.js
@@ -1,5 +1,10 @@
 Template.legoCompositionBlock.events({
 	'click .x': function(e) {
-    
+    const query_id = $(e.target).parent().attr('queryId');
+    const lego_id = $(e.target).parent().attr('sublegoId');
+
+    Queries.update(query_id, {
+      $pull: {"subLegos": lego_id}
+    });
   }
 })
