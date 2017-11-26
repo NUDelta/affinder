bfc9a6609be6c5734e236190bc1925f9e723a92e
youralien <ryanlouie2021@u.northwestern.edu>
2017-11-26 16:57:30 -0500

    $text search working

--- client/templates/block_search_results.js
+++ client/templates/block_search_results.js
@@ -1,6 +1,20 @@
 Template.blockSearchResults.helpers({
   searchResults: function(queryId, searchQuery) {
-    // TODO: use searchQuery to filter down all the existing blocks
-    return Queries.find({_id: { $not: queryId }}).fetch();
+    Meteor.subscribe("blockSearch", searchQuery);
+
+    // We cannot re-use the query as MiniMongo does not support the $text operator!
+    // Instead of resorting to a Meteor method we can hack around it by relying on an extra
+    // ad-hoc collection containing the sorted ids ...
+    const key = JSON.stringify(searchQuery);
+    const result = Queries.BlockSearchResults.findOne(key);
+    const idsInSortOrder = result.results.filter(id => queryId !== id);
+    const blocksInSortedOrder = idsInSortOrder.map(id => Queries.findOne(id));
+    return blocksInSortedOrder;
   }
 })
+
+if (Meteor.isServer) {
+  Queries._ensureIndex({
+    "query": "text"
+  });
+}
--- server/publications.js
+++ server/publications.js
@@ -14,6 +14,25 @@ Meteor.publish('weatherFeatures', function() {
 Meteor.publish('movementFeatures', function() {
   return MovementFeatures.find();
 })
+
+Meteor.publish("blockSearch", function(searchValue) {
+  check(searchValue, String)  // Incredibly important for this callback
+  const logResults = Queries.find(
+    {$text: {$search: searchValue} }
+  );
+
+  // This is a hack to work around the lack of $text support on the client side Minimongo lib
+  // without which we would have trouble recreating the search results on the client
+  const key = JSON.stringify(searchValue); // key that is unique per user and query
+  const resultIds = logResults.map(e => e._id);
+  Queries.BlockSearchResults.upsert(key, {results: resultIds});
+  console.log("key", key)
+
+  // check(Queries.BlockSearchResults.find({_id: key}))
+  // publish the results to the client side
+  return Queries.BlockSearchResults.find(key); // ensures the client side can get the indices in the right sort order
+
+});
  // on the server
 // Meteor.publish('posts', function() {return Posts.find({flagged: false});
 // });
