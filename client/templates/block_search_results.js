7286d19b397b89935b0ed3e40e4808733518943c
youralien <ryanlouie2021@u.northwestern.edu>
2017-11-26 23:18:12 -0600

    Search Prebuilt Blocks based on form input working!

--- client/templates/block_search_results.js
+++ client/templates/block_search_results.js
@@ -1,15 +1,31 @@
+Template.blockSearchResults.onCreated(function() {
+  // by default, use the current parent block query
+  // as input to the prebuilt block search
+  Session.set("searchInputText", this.data.query);
+});
+
 Template.blockSearchResults.helpers({
-  searchResults: function(queryId, searchQuery) {
-    Meteor.subscribe("blockSearch", searchQuery);
+  searchInputText: function() {
+    Session.get("searchInputText");
+  },
+
+  searchResults: function(queryId) {
+    Meteor.subscribe("blockSearch", Session.get("searchInputText"));
 
     // We cannot re-use the query as MiniMongo does not support the $text operator!
     // Instead of resorting to a Meteor method we can hack around it by relying on an extra
     // ad-hoc collection containing the sorted ids ...
-    const key = JSON.stringify(searchQuery);
+    const key = JSON.stringify(Session.get("searchInputText"));
     const result = Queries.BlockSearchResults.findOne(key);
     const idsInSortOrder = result.results.filter(id => queryId !== id);
     const blocksInSortedOrder = idsInSortOrder.map(id => Queries.findOne(id));
     return blocksInSortedOrder;
+  }
+})
+
+Template.blockSearchResults.events({
+  'input #searchResultsInput': function(e, template) {
+    Session.set("searchInputText", e.target.value);
   }
 })
 
