e56d2f4ddad118cd236530eea61aa6e7bceba825
youralien <ryanlouie2021@u.northwestern.edu>
2017-12-13 14:23:42 -0800

    Refactored flow of start page
    Summary:
    1) New Situation is separated from Past Situations
    2) New Situation front-end encourages scaffolding of the Who/What/Where/When

--- client/templates/natural_language_input.js
+++ client/templates/natural_language_input.js
@@ -1,7 +1,35 @@
 Template.naturalLanguageInput.helpers({
-  'submitButtonText': function() {
-    return Session.get("submitNaturalLanguageInputButtonText") || "Submit";
+  submitButtonText() {
+    return Session.get("submitNaturalLanguageInputButtonText") || "Go to Editor";
+  },
+  whoPrompt() {
+    return "Ping who?";
+  },
+  whoPlaceholder() {
+    return "e.g. Loved ones that if they were physically closer, you would talk to everyday";
+  },
+  whatPrompt() {
+    return "What are they doing?";
+  },
+  whatPlaceholder() {
+    return "e.g. Commuting by walking, train, or car";
+  },
+  wherePrompt() {
+    return "Where are they located?";
+  },
+  wherePlaceholder() {
+    return "e.g. The loved ones are living in different cities";
+  },
+  whenPrompt() {
+    return "When is this occuring?";
+  },
+  whenPlaceholder() {
+    return "e.g. Before Work, After Work";
+  },
+  readyText() {
+    return "When ready, continue constructing a situational trigger a mobile phone can sense.";
   }
+
 })
 
 Template.naturalLanguageInput.events({
