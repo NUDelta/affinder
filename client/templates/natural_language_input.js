3c5fdf8548833440c86e13489a254cc145ddfa0a
youralien <ryanlouie2021@u.northwestern.edu>
2018-01-11 14:27:40 -0600

    Gave more control of workflow in the same page, without having to exit
    Summary:
    - Add sublegos without having to define them first. Before you can define a new sublego, but it would immediately have you define the sublego and take you to a separate page.
    - Adjusted Styling so the page is larger, to fit in the interfaces for a one-page interaction
    - Can change what/where/when within the single page interface, to give more flexibility if you update these semantics during creation
    - Defined the overall query as the joining of the 3Ws
    - TODO: it's not natural that the 3Ws are empty, when one defines sublegos.  There are several fixes for merging the child situation representation with the couple word phrase

--- client/templates/natural_language_input.js
+++ client/templates/natural_language_input.js
@@ -1,46 +1,63 @@
 Template.naturalLanguageInput.helpers({
-  submitButtonText() {
-    return "Go to Editor";
+  header() {
+    // if inside editor expr1, else expr2
+    return (this.hasOwnProperty("_id")) ? "" : "New Situation";
   },
-  whoPrompt() {
-    return "Ping who?";
+  explanation() {
+    return "IF person can do {WHAT/ACTIVITY} AND located at {WHERE/LOCATION} AND during {WHEN/TIME}";
   },
-  whoPlaceholder() {
-    return "e.g. Loved ones that if they were physically closer, you would talk to everyday";
+  instructions() {
+    // if inside editor expr1, else expr2
+    if (this.hasOwnProperty("_id")) {
+      return "Describe the WHAT/WHERE/WHEN of the Situation";
+    } else {
+      return "Describe the WHAT/WHERE/WHEN of the Situation";
+    }
   },
   whatPrompt() {
-    return "What are they doing?";
+    return "WHAT/ACTIVITY";
   },
   whatPlaceholder() {
-    return "e.g. Commuting by walking, train, or car";
+    return "";
   },
   wherePrompt() {
-    return "Where are they located?";
+    return "WHERE/LOCATION";
   },
   wherePlaceholder() {
-    return "e.g. The loved ones are living in different cities";
+    return "";
   },
   whenPrompt() {
-    return "When is this occuring?";
+    return "WHEN/TIME";
   },
   whenPlaceholder() {
-    return "e.g. Before Work, After Work";
+    return "sunrise, autumn, during finals period";
   },
   readyText() {
-    return "When ready, continue constructing a situational trigger a mobile phone can sense.";
+    if (this.hasOwnProperty("_id")) {
+      return ""
+    } else {
+      return "When ready, continue constructing a situational trigger a mobile phone can sense.";
+    }
+  },
+  submitButtonText() {
+    // if inside editor expr1, else expr2
+    return (this.hasOwnProperty("_id")) ? "Update" : "Go to Editor";
   }
-
 });
 
 Template.naturalLanguageInput.events({
   'submit form': function(e, template) {
     e.preventDefault();
 
+    const what = $(e.target).find('[name=what]').val();
+    const where = $(e.target).find('[name=where]').val();
+    const when = $(e.target).find('[name=when]').val();
+
     var situation = {
-      who: $(e.target).find('[name=who]').val(),
-      what: $(e.target).find('[name=what]').val(),
-      where: $(e.target).find('[name=where]').val(),
-      when: $(e.target).find('[name=when]').val(),
+      what: what,
+      where: where,
+      when: when,
+      query: ([what, where, when].join(" "))
     };
 
     if (template.data) {
