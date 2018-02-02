ef5421be1a274843ac85d0f294f9aa7c00563429
youralien <ryanlouie2021@u.northwestern.edu>
2018-02-01 19:54:33 -0600

    yelp place recommendations work for this new workspace interface; transforming it to blockly WIP

--- client/templates/feature_discovery.js
+++ client/templates/feature_discovery.js
@@ -5,6 +5,75 @@ Template.searchBar.events({
     var newTree = defaultToolbox();
     newTree["placeCategories"] = "";
     WORKSPACE.updateToolbox(stringifyToolboxTree(newTree));
+  },
+
+  'submit form#blockSearch': function(e) {
+    e.preventDefault();
+
+    const queryText = $(e.target).find('[name=search]').val();
+    alert(queryText);
+
+    const queryId = Queries.insert({ query: queryText });
+    alert(queryId);
+    Session.set('yelpLoading', true);
+    Session.set('currentQueryId', queryId);
+    Meteor.call('updateYelpPlaceCategories',
+      {
+        _id: queryId,
+        query: queryText
+      }, function (error, data) {
+        Session.set('yelpLoading', false);
+        if (error) {
+          return alert(error.reason)
+        }
+      });
   }
+});
+
+function resolveAllAndExcludedCats(allCats, excludeCats) {
+  allCats = allCats.map(function(item) {
+    if (item instanceof Array) {
+      // item: [category-name, tfidf-weight]
+      return item[0];  // grab only category-name
+    } else {
+      // item: category-name
+      return item;
+    }
+  });
+  if (excludeCats) {
+    return allCats.filter(function(item) {
+      return !excludeCats.includes(item);
+    });
+  }
+  else {
+    return allCats;
+  }
+}
+
+Template.featureDiscovery.helpers({
+  'yelpLoading': function() {
+    return Session.get('yelpLoading');
+  },
+
+  'currentQueryId': function() {
+    return Session.get('currentQueryId');
+  },
+
+  'includedCategories': function(queryId) {
+    obj = Queries.findOne(queryId);
+    return resolveAllAndExcludedCats(obj.categories, obj.excluded_categories);
+  }
+});
+
+Template.featureDiscovery.events({
+  'click .x': function(e) {
+    const cat2rm = $(e.target).parent().attr('placeCategory');
+
+    Queries.update(Session.get('currentQueryId'), {
+      $addToSet: {excluded_categories: cat2rm}
+    });
+  },
+
 })
+
 
