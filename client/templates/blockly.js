449ad51b14168fedeab1472fccb6eb0426ebd9fd
youralien <ryanlouie2021@u.northwestern.edu>
2018-02-08 23:44:53 -0600

    added daytime/nighttime and america timezones

--- client/templates/blockly.js
+++ client/templates/blockly.js
@@ -176,6 +176,12 @@ defaultToolboxTime = function() {
       <field name="VAR">sunset</field>
     </block>
     <block type="variables_get">
+      <field name="VAR">daytime</field>
+    </block>
+    <block type="variables_get">
+      <field name="VAR">nighttime</field>
+    </block>
+    <block type="variables_get">
       <field name="VAR">hour</field>
     </block>
     <block type="variables_get">
@@ -202,7 +208,18 @@ defaultToolboxTime = function() {
     <block type="variables_get">
       <field name="VAR">sunday</field>
     </block>
-
+    <block type="variables_get">
+      <field name="VAR">america_los_angeles</field>
+    </block>
+    <block type="variables_get">
+      <field name="VAR">america_denver</field>
+    </block>
+    <block type="variables_get">
+      <field name="VAR">america_chicago</field>
+    </block>
+    <block type="variables_get">
+      <field name="VAR">america_new_york</field>
+    </block>
     <block type="logic_compare">
       <value name="A">
         <block type="variables_get">
