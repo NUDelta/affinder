d1a215afcc618a79456eb82046f151491db289be
youralien <ryanlouie2021@u.northwestern.edu>
2018-04-18 14:37:42 -0500

    added example situations from uist paper

---
+++ client/templates/example_situations.js
@@ -0,0 +1,12 @@
+Template.exampleSituations.helpers({
+  situationPrompts: function() {
+    return [
+      "People are enjoying winter (fun winter activities and winter weather)",
+      "Start a snowball or food fight (plenty of snow and school cafeterias)",
+      "Awesome situations to throw a paper airplane (open areas like parks without many people, weather not disruptive, while there is daylight",
+      "Harry Potter looks up at the large castle (gothic-looking buildings, model-sized castles)",
+      "Parkour: Jumping over puddles (rain and asphalt)",
+      "Waiting in Line (waiting for fun, transit, or crowded long waits)"
+    ];
+  }
+})
