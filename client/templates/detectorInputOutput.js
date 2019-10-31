f6ee7609f40377f12c26c0713fda480d248e4c79
youralien <ryanlouie2021@u.northwestern.edu>
2019-10-31 14:22:18 -0500

    Made room in UI for SimulationUI

--- client/templates/detectorInputOutput.js
+++ client/templates/detectorInputOutput.js
@@ -1,22 +1,22 @@
 import {Detectors} from "../../lib/collections/collections";
 
-Template.workspace.events({
+Template.detectorInputOutput.events({
   'submit form': function(e) {
     e.preventDefault();
-    code = $('#compiledBlockly').val();
-    var splitJS = splitVarDeclarationAndRules(code)
+    let code = $('#compiledBlockly').val();
+    var splitJS = splitVarDeclarationAndRules(code);
 
-    detector = {
+    let detector = {
       description: $('input[name=detectorname]').val(),
       variables: splitJS[0],
       rules: splitJS[1]
-    }
+    };
 
     if (Session.get('detectorId')) {
       Detectors.update(Session.get('detectorId'), detector);
     } else {
-      detectorId = Detectors.insert(detector);
+      let detectorId = Detectors.insert(detector);
       Session.set('detectorId', detectorId);
     }
   }
-})
+});
