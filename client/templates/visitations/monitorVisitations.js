dfe935f4f77adee9309a72b29d64655f8410fbc5
youralien <ryanlouie2021@u.northwestern.edu>
2022-08-26 16:43:23 -0500

    Visitation monitor: story view, and progress bars
    Summary:
    - Resolves #22 by making a story-based form for inputting parameters of
      the model
    - Uses progressbars to show the probability of visitation for all cities
    - TODO: add ability to save progress; and show the relative change with
      stacked bars

--- client/templates/visitations/monitorVisitations.js
+++ client/templates/visitations/monitorVisitations.js
@@ -18,6 +18,12 @@ Template.monitorVisitations.onRendered(function() {
   Session.set('daysPeriod', daysPeriod);
   const numUsersInCity = Number(document.getElementById('numUsersInCity').value);
   Session.set('numUsersInCity', numUsersInCity);
+  let probabilityOfVisitationHistory = Object.assign({}, numFSQUsersPerCity);
+  Object.keys(probabilityOfVisitationHistory).forEach((city) => {
+    probabilityOfVisitationHistory[city] = []
+  });
+  Session.set('probabilityOfVisitationHistory', probabilityOfVisitationHistory);
+  Session.set('probMaxReferenceLimit', 0.5);
 });
 
 Template.monitorVisitations.events({
@@ -49,9 +55,42 @@ Template.monitorVisitations.events({
     uid = event.target.value;
     Session.set('uidForSimulation', uid);
   },
+  'change #probMaxReferenceLimit'(event) {
+    limit = event.target.value;
+    Session.set('probMaxReferenceLimit', limit);
+  },
+  'click #saveProbabilityHistory'(event) {
+    // TODO(rlouie): finish function
+    let probabilityOfVisitationHistory = Session.get('probabilityOfVisitationHistory');
+    Object.keys(probabilityOfVisitationHistory).forEach((city) => {
+      const numTotalCheckins = totalCheckins(city);
+      const numTotalUsersInCity = numFSQUsersPerCity[city];
+      const totalDays = 365 // data collected over 1 - 1.5 years
+      const daysPeriod = Session.get('daysPeriod');
+      const numUsersInCity = Session.get('numUsersInCity');
+      const probability = calculateProbabilityOfVisitation(numTotalCheckins, numTotalUsersInCity, totalDays, daysPeriod, numUsersInCity);
+      probabilityOfVisitationHistory[city].push(probability);
+    });
+    Session.set('probabilityOfVisitationHistory', probabilityOfVisitationHistory);
+  },
 
 
 });
+
+const calculateProbabilityOfVisitation = (numTotalCheckinsInCity, numTotalUsersInCity, totalDays, daysPeriod, numUsersInCity) => {
+  // e.g., ~2000 checkins at churches
+  // you have 52 periods (a week) in a year (360 / 7).
+  // So you have ~40 people visiting a church on any given week, for the entire population in Foursquare.
+  const expectedNumVisitsForPeriodForAllUsers = numTotalCheckinsInCity / (totalDays / daysPeriod)
+
+  // But imagine we have a user population that is a fraction of FSQ, e.g., 1%
+  // e.g., 10 people in our deploy population, but FSQ had 1000 people.
+  // So what is expected number of people from YOUR population to visit churches?
+  // Well 0.01 * 40 = 0.4 people visiting a church on any given week.
+  // Or 40% chance someone will visit a church on any given week.
+  const expectedVisitsForUserSubset = (numUsersInCity / numTotalUsersInCity) * expectedNumVisitsForPeriodForAllUsers;
+  return Math.min(1, expectedVisitsForUserSubset);
+}
 
 Template.monitorVisitations.helpers({
   // ------ CITY-BASED MODEL HELPERS ------
@@ -67,30 +106,29 @@ Template.monitorVisitations.helpers({
     return totalCheckins(city);
   },
 
-  probabilityOfVisitation() {
-    const city = Session.get('cityForSimulation');
+  probabilityOfVisitation(city) {
     const numTotalCheckins = totalCheckins(city);
-    if (!numTotalCheckins) {
-      return;
-    }
     const numTotalUsersInCity = numFSQUsersPerCity[city];
-    // data collected over 1 - 1.5 years
-    const totalDays = 365
+    const totalDays = 365 // data collected over 1 - 1.5 years
     const daysPeriod = Session.get('daysPeriod');
     const numUsersInCity = Session.get('numUsersInCity');
 
-    // e.g., ~2000 checkins at churches
-    // you have 52 periods (a week) in a year (360 / 7).
-    // So you have ~40 people visiting a church on any given week, for the entire population in Foursquare.
-    const expectedNumVisitsForPeriodForAllUsers = numTotalCheckins / (totalDays / daysPeriod)
+    const prob = calculateProbabilityOfVisitation(numTotalCheckins, numTotalUsersInCity, totalDays, daysPeriod, numUsersInCity);
+    const result = prob * 100;
+    return result.toPrecision(2);
+  },
 
-    // But imagine we have a user population that is a fraction of FSQ, e.g., 1%
-    // e.g., 10 people in our deploy population, but FSQ had 1000 people.
-    // So what is expected number of people from YOUR population to visit churches?
-    // Well 0.01 * 40 = 0.4 people visiting a church on any given week.
-    // Or 40% chance someone will visit a church on any given week.
-    const expectedVisitsForUserSubset = (numUsersInCity / numTotalUsersInCity) * expectedNumVisitsForPeriodForAllUsers;
-    return Math.min(1, expectedVisitsForUserSubset.toPrecision(2));
+  progressBarWidth(city) {
+    const numTotalCheckins = totalCheckins(city);
+    const numTotalUsersInCity = numFSQUsersPerCity[city];
+    const totalDays = 365 // data collected over 1 - 1.5 years
+    const daysPeriod = Session.get('daysPeriod');
+    const numUsersInCity = Session.get('numUsersInCity');
+
+    const prob = calculateProbabilityOfVisitation(numTotalCheckins, numTotalUsersInCity, totalDays, daysPeriod, numUsersInCity);
+    const referenceValue = Session.get('probMaxReferenceLimit');
+    const result = prob / referenceValue * 100;
+    return result.toPrecision(2);
   },
 
   // ------ PERSON-BASED HELPERS -------
