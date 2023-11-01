5ea46fd03634ee39e43fc36f88013ac4dc5bc737
Ay Nio <agidummy2@gmail.com>
2023-10-31 19:06:45 -0500

    Initial Chat interface

---
+++ client/templates/chat_interface.js
@@ -0,0 +1,25 @@
+Template.chatInterface.events({
+    'click #send-button': async function() {
+      const userInput = document.getElementById('user-input').value;
+      const chatBox = document.getElementById('chat-box');
+  
+      // Append user message to chat box
+      chatBox.innerHTML += `<div>User: ${userInput}</div>`;
+  
+      // Clear the input field
+      document.getElementById('user-input').value = '';
+  
+      // Make an API call to the backend
+      const response = await fetch('http://localhost:4000/query', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ message: userInput }),
+      });
+  
+      const data = await response.json();
+  
+      // Append bot's reply to chat box
+      chatBox.innerHTML += `<div>Bot: ${data}</div>`;
+    },
+  });
+  
